4c4307a1cdf8310daaec6b5b214cddeed8579c5c
VSPrajwa <Prajwal.vs@unisys.com>
2025-02-21 17:59:19 +0530

    first commit

--- src/utils/data/contactsData.ts
+++ src/utils/data/contactsData.ts
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'ishaqjaveed1@gmail.com',
-    phone: '+923217281104',
-    address: 'Sahiwal, Punjab, Pakistan',
+    email: 'prajwalvs172@gmail.com',
+    phone: '+91-9663941225',
+    address: 'Mathikere, Bangalore, Karnataka, India',
     github: 'https://github.com/javeedishaq',
     facebook: 'https://web.facebook.com/javeedishaq',
     linkedIn: 'https://www.linkedin.com/in/javeed-ishaq/',
     twitter: 'https://twitter.com/javeedishaq',
     stackOverflow: 'https://stackoverflow.com/users/4778545/javeed-ishaq',
-    devUsername: "javeedishaq"
+    devUsername: "prajwalvs"
 }
